a4d6c49950c817f93d04dd7a381b3434fcf3f106
Harry Bleckert <Harry@Bleckert.com>
2024-01-25 20:10:49 +0800

    no comments

--- db/upgrade.php
+++ db/upgrade.php
@@ -442,12 +442,18 @@ function xmldb_evaluation_upgrade($oldversion) {
         }
         upgrade_mod_savepoint(true, $newversion, 'evaluation');
     }
+
+
     $newversion = 2024012501;
     if ($oldversion < $newversion) {
         $table = new xmldb_table('evaluation_users_la');
         $field = new xmldb_field('teacherids', XMLDB_TYPE_CHAR, '600', null, null, null, '');
         if ($dbman->field_exists($table, $field)) {
             $dbman->rename_field($table, $field,"courseids");
+        }
+        $field = new xmldb_field('courseids', XMLDB_TYPE_CHAR, '600', null, null, null, '');
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
         }
         upgrade_mod_savepoint(true, $newversion, 'evaluation');
     }
