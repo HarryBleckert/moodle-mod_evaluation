1cf0413f58daa54e7eab3f02eef874c7b5902de5
Harry Bleckert <Harry@Bleckert.com>
2024-02-11 14:40:38 +0530

    no comments

--- compare_results.inc.php
+++ compare_results.inc.php
@@ -1484,7 +1484,7 @@ function evaluation_compare_results($evaluation, $courseid = false,
             $tags["SqAvg"] = "";
         }
     }
-    if (count($rowsA) < 2) { // AND !$filter
+    if (count($rowsA) < 2 AND !$filter ){
         print "\n<script>document.getElementById('showFilter').style.display='none';</script>\n";
         unset($data["averageF"]);
     }
