3a8fe2fee4356b74d92fe265eb5aa55925fdc57d
Harry Bleckert <Harry@Bleckert.com>
2024-02-11 23:47:16 +0530

    no comments

--- compare_results.inc.php
+++ compare_results.inc.php
@@ -782,14 +782,14 @@ function evaluation_compare_results($evaluation, $courseid = false,
         $allKey = "teacherid";
         $allKeyV = "teacherid";
         $aFilter = "teacherid >0"; // .$cosPrivileged_filter;
-        $evaluationResults = safeCount($DB->get_records_sql("SELECT teacherid AS teacherid, count(*) AS count
+        $evaluationResults = safeCount($DB->get_records_sql("SELECT teacherid AS teacherid, id, count(*) AS count
 											 FROM {evaluation_completed}
 											 WHERE evaluation=$evaluation->id AND $aFilter $subqueryC
 											 GROUP BY teacherid ORDER BY teacherid"));
-        $allResults = $DB->get_records_sql("SELECT DISTINCT ON (teacherid) teacherid AS teacherid, courseid, count(*) AS count
+        $allResults = $DB->get_records_sql("SELECT teacherid AS teacherid, courseid, count(*) AS count
 											 FROM {evaluation_completed}
 											 WHERE evaluation=$evaluation->id $filter $subqueryC
-											 ORDER BY teacherid");
+											 GROUP BY teacherid ORDER BY teacherid");
         $evaluatedResults = 0;
         foreach ($allResults as $allResult) {
 
