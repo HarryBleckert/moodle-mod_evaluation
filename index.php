5d45a8dd7da7c36f242019a29fc1c2bf5dd53330
Harry Bleckert <Harry@Bleckert.com>
2024-10-16 16:01:46 +0800

    no comments

--- index.php
+++ index.php
@@ -140,7 +140,7 @@ foreach ($evaluations as $evaluation) {
     $tablerow = new html_table_row();
     $tablerow->attributes['tag_sort'] = $evaluation->tag_sort;
     $tablerow->attributes['style'] = "display:none;";
-    if ($current_tag !== $evaluation->tag_sort){
+    if ($current_tag != $evaluation->tag_sort){
         $current_tag = $evaluation->tag_sort;
         $tablerow->attributes['style'] = "display:inline;";
     }
