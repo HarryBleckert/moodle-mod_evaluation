375c31a06b4ce2b7c33149962402312d192c3052
Harry Bleckert <Harry@Bleckert.com>
2024-11-11 22:56:08 +0900

    no comments

--- db/tasks.php
+++ db/tasks.php
@@ -26,6 +26,7 @@ $tasks = [
         [
                 'classname' => 'mod_evaluation\task\cron_task',
                 'blocking' => 0,
+                'disabled' => 0,
                 'minute' => '30',
                 'hour' => '17',
                 'day' => '*',
