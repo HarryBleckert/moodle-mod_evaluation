daa48a06cfc645df6ff309a17bfbd45f90d6ff09
Harry Bleckert <Harry@Bleckert.com>
2024-11-13 22:44:52 +0900

    no comments

--- locallib.php
+++ locallib.php
@@ -4792,7 +4792,8 @@ return true;
     $timenow = time();
     $task = \core\task\manager::get_scheduled_task(mod_evaluation\task\cron_task::class);
     $lastruntime = $task->get_last_run_time();
-    mtrace("Time now: ".date("d.m,Y H:i:s",$timenow). " - last runtime: ".date("d.m,Y H:i:s",$lastruntime));
+    mtrace("Time now: ".date("d.m,Y H:i:s",$timenow). " - last runtime: "
+            .date("d.m,Y H:i:s",$lastruntime));
 
     $evaluations = $DB->get_records_sql("SELECT * from {evaluation}");
     // only run in test mode
@@ -4809,7 +4810,7 @@ return true;
 
             $reminders = $evaluation->reminders;
             if (empty($reminders)){
-                mtrace('Evaluation '$evaluation->name': Sending reminders to teachers and students');
+                mtrace("Evaluation '$evaluation->name': Sending reminders to teachers and students");
                 ev_send_reminders($evaluation, "teacher", $noreplies, $test, $cli, $verbose, $cronjob);
                 ev_send_reminders($evaluation, "student", $noreplies, $test, $cli, $verbose, $cronjob);
                 break;
