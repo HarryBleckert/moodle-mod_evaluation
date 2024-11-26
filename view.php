a1ea7d143aa1ad4c65196a437b7cf8b489627824
Harry Bleckert <Harry@Bleckert.com>
2024-11-27 02:26:33 +0900

    no comments

--- view.php
+++ view.php
@@ -742,7 +742,7 @@ if (is_siteadmin()) {
         ev_shuffle_completed_userids($evaluation, true);
     }
 
-    if ($CFG->dbname == 'moodle_staging' AND isset($_GET['renumber'])) {
+    if (isset($_GET['renumber'])) {
         evaluation_renumber_items($evaluation->id);
     }
     //if( !safeCount( $DB->get_records_sql("SELECT * FROM {evaluation_item}
