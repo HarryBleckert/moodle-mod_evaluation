68acc324901c62e2859d9db592183be73d2fd2ff
Harry Bleckert <Harry@Bleckert.com>
2024-11-30 15:27:31 +0900

    no comments

--- locallib.php
+++ locallib.php
@@ -4818,7 +4818,7 @@ function ev_cron() {
     // $task = \core\task\manager::get_scheduled_task(mod_evaluation\task\cron_task::class);
     // $lastruntime = $task->get_last_run_time();
     // mtrace("Time now: ".date("d.m,Y H:i:s",$timenow). " - last runtime: "
-            .date("d.m,Y H:i:s",$lastruntime));
+    //        .date("d.m,Y H:i:s",$lastruntime));
 
     $evaluations = $DB->get_records_sql("SELECT * from {evaluation}");
     // only run in test mode
