938b5ca88c487914148115823706dd158f6d480a
Harry Bleckert <Harry@Bleckert.com>
2024-12-11 16:10:29 +0800

    no comments

--- view.php
+++ view.php
@@ -324,8 +324,7 @@ $msg_teachers = "Guten Tag $fullname<br>
                 ? "nehmen. Bitte motivieren Sie die Studierenden zur Teilnahme" : "genommen haben") . ".<br>
                         Für Ihre eigenen Kurse können Sie die statistische Auswertung einsehen, sobald $minResults Abgaben vorliegen. 
                         Ab $minResultsText Abgaben können Sie auch die Textantworten einsehen.<br>\n
-                        Herzliche Grüße<br>$evaluation->signature<br>\n"
-                        . $showPrivDocu;
+                        Herzliche Grüße<br>$evaluation->signature<br>\n";
 if ($is_open) {
     $days = remaining_evaluation_days($evaluation);
     $alert = "";
