e187146899f695b48a8f675ff5d554aff306916d
Harry Bleckert <Harry@Bleckert.com>
2025-01-19 20:50:24 +0800

    no comments

--- lang/zh_cn/evaluation.php
+++ lang/zh_cn/evaluation.php
@@ -36,7 +36,7 @@ $string['index_group_by_tag'] = '群组';
 // 发送提醒
 $string['send_reminders_noreplies_teachers'] = '仅针对提交次数少于 {$a->min_results_text} 的教师。';
 $string['send_reminders_noreplies_students'] = '仅限尚未参加评估的学生。';
-$string['send_reminders_pmsg'] = '今天，有关正在进行的评估的提醒电子邮件已发送给所有课程正在参与评估的{$a->role}。下面您可以看到一个例子。 '；
+$string['send_reminders_pmsg'] = '今天，有关正在进行的评估的提醒电子邮件已发送给所有课程正在参与评估的{$a->role}。下面您可以看到一个例子。 ';
 $string['send_reminders_remaining'] = '距离 {$a->lastEvaluationDay} 仅剩 {$a->remaining_evaluation_days} 天';
 $string['send_reminders_students'] = '{$a->testmsg} <p>您好 {$a->fullname}
 <p>请{$a->also}参与{$a->reminder}正在进行的评估<br>
@@ -66,30 +66,30 @@ $string['send_reminders_students'] = '{$a->testmsg} <p>您好 {$a->fullname}
     {$a->签名}
     {$a->signum}';
 
-    $string['send_reminders_no_replies'] = '您的{$a->distinct_s}名学生尚未参加。 '；
-    $string['send_reminders_few_replies'] = '目前为止，只有 {$a->replies} 个 {$a->submissions} 来自您的 {$a->distinct_s} 名学生。 '；
-    $string['send_reminders_many_replies'] = '到目前为止您的 {$a->distinct_s} 名学生共提交了 {$a->replies} 条回复';
-    $string['send_reminders_privileged'] = '您收到这封电子邮件是为了了解信息，因为您被授权查看此次评估的评估结果。';
-    // 结束发送提醒
+$string['send_reminders_no_replies'] = '您的{$a->distinct_s}名学生尚未参加。 '；
+$string['send_reminders_few_replies'] = '目前为止，只有 {$a->replies} 个 {$a->submissions} 来自您的 {$a->distinct_s} 名学生。 '；
+$string['send_reminders_many_replies'] = '到目前为止您的 {$a->distinct_s} 名学生共提交了 {$a->replies} 条回复';
+$string['send_reminders_privileged'] = '您收到这封电子邮件是为了了解信息，因为您被授权查看此次评估的评估结果。';
+// 结束发送提醒
 
-    // 翻译功能，Magik 为 Berthe 工作
-    $string['evaluation_of_courses'] = '课程评估';
-    $string['by_students'] = '由学生';
-    $string['of_'] = 'des';
-    $string['for_'] = '为了那个';
-    $string['sose_'] = '夏季学期';
-    $string['wise_'] = '冬季学期';
-    // 结束翻译函数
+// 翻译功能，Magik 为 Berthe 工作
+$string['evaluation_of_courses'] = '课程评估';
+$string['by_students'] = '由学生';
+$string['of_'] = 'des';
+$string['for_'] = '为了那个';
+$string['sose_'] = '夏季学期';
+$string['wise_'] = '冬季学期';
+// 结束翻译函数
 
-    // 图形库
-    $string['show_graphic_data'] = '显示图形数据';
-    $string['hide_graphic_data'] = '隐藏图形数据';
-    // 结束图库
+// 图形库
+$string['show_graphic_data'] = '显示图形数据';
+$string['hide_graphic_data'] = '隐藏图形数据';
+// 结束图库
 
-    // 视图.php
-    $string['是'] = '是';
-    $string['是'] = '是';
-    $string['teamteachingtxt'] = '在团队教学的研讨会上，讲师将接受单独评估。';
-    $string['is_WM_disabled'] = '继续教育硕士学位课程被排除在外。';
-    $string['siteadmintxt'] = '管理员因此';
-    $string['andrawdata'] = '和原始数据';
+// 视图.php
+$string['是'] = '是';
+$string['是'] = '是';
+$string['teamteachingtxt'] = '在团队教学的研讨会上，讲师将接受单独评估。';
+$string['is_WM_disabled'] = '继续教育硕士学位课程被排除在外。';
+$string['siteadmintxt'] = '管理员因此';
+$string['andrawdata'] = '和原始数据';
