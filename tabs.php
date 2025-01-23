769c29ad79c718b71a527801b07e97a6a2e3bd66
Harry Bleckert <Harry@Bleckert.com>
2025-01-24 03:59:30 +0800

    no comments

--- tabs.php
+++ tabs.php
@@ -258,7 +258,7 @@ if ($evaluation->course != SITEID AND
 if (safeCount($row) > 0) {
     $tabs[] = $row;
     echo "\n" . '<div 
-        style="display:inline;.tabtree {text-decoration: underline;font-weight: bold;a:link {text-decoration: none;font-weight: normal;}}" 
+        style="display:inline;.tabtree text-decoration: underline;font-weight: bold;a:link {text-decoration: none;font-weight: normal;}" 
         class="d-print-none">';
     print_tabs($tabs, $current_tab, $inactive, $activated);
     echo "</div>\n";
