39d2c93e9323ec31916a430a081dcd185001b871
Harry Bleckert <Harry@Bleckert.com>
2025-01-29 15:56:56 +0800

    no comments

--- compare_results.inc.php
+++ compare_results.inc.php
@@ -1802,9 +1802,12 @@ function evaluation_compare_results($evaluation, $courseid = false,
         // Output results
         echo "<br><hr>\n";
         echo "Total submissions with unanswered questions for Evaluation ID $evaluationid: " . $total_unanswered_submissions . "<br>";
+        $total_unanswered_questions = 0;
         foreach ($submissions as $submission) {
+            $total_unanswered_questions ++;
             echo "Submission ID: {$submission->submission_id}, Unanswered Questions: {$submission->unanswered_questions} <br>";
         }
+        echo "<hr>Total unanswered questions for Evaluation ID $evaluationid: " . $total_unanswered_questions . "<br>";
         echo "<hr><br>\n";
 
         $noAnswerSum = $ignoredAnswerSum = 0;
