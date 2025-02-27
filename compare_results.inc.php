02ae2ab09dcec0a0d22f80886e98abf268bd31ec
Harry Bleckert <Harry@Bleckert.com>
2025-02-28 01:34:44 +0800

    no comments

--- compare_results.inc.php
+++ compare_results.inc.php
@@ -1741,7 +1741,7 @@ function evaluation_compare_results($evaluation, $courseid = false,
             };
 
             //Chart.helpers.color(color).lighten(0.2);
-            jQuery('#evDataTable').html(dataToTable(<?php echo json_encode($JSdataFull); ?>));
+            jQuery('#evDataTable').html(dataToTable(<?php echo json_encode(Table); ?>));
 
         </script>
         <?php
