bb8807bdfe0e36eb345510d873e82027b48f87fb
Harry Bleckert <Harry@Bleckert.com>
2025-04-15 17:19:34 +0800

    no comments

--- compare_results.inc.php
+++ compare_results.inc.php
@@ -726,9 +726,12 @@ function evaluation_compare_results($evaluation, $courseid = false,
             $course_of_studiesID =
                     evaluation_get_course_of_studies_id_from_evc($id, $allResult->course_of_studies, $evaluation);
             $allCosIDs[] = $course_of_studiesID;
-            if (defined('EVALUATION_OWNER') ||
+            /*if (defined('EVALUATION_OWNER') ||
                     ($cosPrivileged ?!empty($_SESSION['CoS_privileged'][$USER->username][$allResult->course_of_studies])
+                            :evaluation_is_my_cos($myEvaluations, $allResult->course_of_studies))) {*/
+            if ( ($cosPrivileged ?!empty($_SESSION['CoS_privileged'][$USER->username][$allResult->course_of_studies])
                             :evaluation_is_my_cos($myEvaluations, $allResult->course_of_studies))) {
+
                 $links = '<a href="analysis_course.php?id=' . $id .
                         '&course_of_studiesID='
                         . $course_of_studiesID
